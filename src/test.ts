9187371a753bd4004a1c02611db5338a63a383a2
alexminden <alexandre@minden.co.jp>
2021-10-22 16:52:04 +0900

    Remove unneeded code

--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,10 @@
 import { createTestKeyring } from '@polkadot/keyring/testing';
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { methods } from "@substrate/txwrapper-polkadot";
 import axios from 'axios';
 
 async function main() {
     const keyring = createTestKeyring();
     const pair = keyring.getPairs();
-    // const pair2 = keyring.getPairs()[4];
     const wsProvider = new WsProvider('ws://127.0.0.1:9944');
     const api = await ApiPromise.create({ provider: wsProvider });
     const now = await api.query.timestamp.now();
@@ -18,44 +16,30 @@ async function main() {
     const txHash = await api.tx.balances.transfer(pair[5].address, 1000).signAndSend(pair[0]);
     console.log(txHash);
     
-    const metadata = await api.rpc.state.getMetadata()
-    // const tx = await api.tx.balances.transfer(pair[5].address, 1000);
-    console.log('Metadata: ' + JSON.stringify(metadata, null, 2));
-    // const unsigned = methods.balances.transferKeepAlive(
-    //     {
-    //         dest: pair2.address,
-    //         value: 5000000000,
-    //     },
-    //     {
-    //         address: pair.address,
-    //         blockHash:
-    //             api.genesisHash,
-    //         blockNumber: 4302222,
-    //         genesisHash:
-    //             api.genesisHash,
-    //         metadataRpc: metadata, // must import from client RPC call state_getMetadata
-    //         nonce: 2,
-    //         specVersion: 1019,
-    //         tip: 0,
-    //         eraPeriod: 64, // number of blocks from checkpoint that transaction is valid
-    //         transactionVersion: 1,
-    //     },
-    //     {
-    //         metadataRpc: metadata,
-    //         registry, // Type registry
-    //     }
-    // );
-
-    // const test = await axios.post(
-    //     "http://127.0.0.1:9944/transaction/",
-    //     `{ "tx": ${}}`,
-    //     {
-    //         headers: {
-    //             'Accept': "text/plain",
-    //             'Content-Type': 'application/json'
-    //         },
-    //     }
-    // );
+    // const metadata = await api.rpc.state.getMetadata();
+    const test = await axios.post(
+        "http://127.0.0.1:9933",
+        {
+            "jsonrpc": "2.0",
+            "method": "runtime_createExtrinsic",
+            "params": [
+                pair[0].address,
+                "Balances",
+                "transfer",
+                {
+                    "dest": pair[5].address,
+                    "value": 50
+                }
+            ],
+            "id": 1
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }
+    );
+    console.log('HTTP: ', test);
 }
 
 main().then(
