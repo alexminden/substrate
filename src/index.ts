995e95cf4d7c6437d29efc22526bc307ca556803
alexminden <alexandre@minden.co.jp>
2021-10-25 18:44:07 +0900

    Finish http transfer

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { createTestKeyring } from '@polkadot/keyring/testing';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { apiTransaction, httpTransaction } from './test';
+
+async function main() {
+    const keyring = createTestKeyring();
+    const pair = keyring.getPairs();
+    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+    const api: ApiPromise = await ApiPromise.create({ provider: wsProvider });
+    const now = await api.query.timestamp.now();
+    for (let i in pair) {
+        const balance = await api.query.system.account(pair[i].address);
+        console.log(`Account ${pair[i].address} balance is ${balance}`);
+    }
+
+    await apiTransaction(pair, api);
+    await httpTransaction(pair);
+}
+
+main().then(
+    () => process.exit(),
+    err => {
+        console.error(err);
+        process.exit(-1);
+    }
+);
--- src/test.ts
+++ src/test.ts
@@ -1,51 +1,86 @@
-import { createTestKeyring } from '@polkadot/keyring/testing';
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import axios from 'axios';
+import { ApiPromise } from '@polkadot/api';
+import {
+    construct,
+    getRegistry,
+    methods,
+} from '@substrate/txwrapper-polkadot';
+import { httpRequest, signWith } from './utils';
+import { KeyringPair } from '@polkadot/keyring/types';
 
-async function main() {
-    const keyring = createTestKeyring();
-    const pair = keyring.getPairs();
-    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
-    const api = await ApiPromise.create({ provider: wsProvider });
-    const now = await api.query.timestamp.now();
-    for(let i in pair) {
-        const balance = await api.query.system.account(pair[i].address);
-        console.log(`Account ${pair[i].address} balance is ${balance}`);
-    }
-
-    const txHash = await api.tx.balances.transfer(pair[5].address, 1000).signAndSend(pair[0]);
-    console.log(txHash);
-    
-    // const metadata = await api.rpc.state.getMetadata();
-    const test = await axios.post(
-        "http://127.0.0.1:9933",
-        {
-            "jsonrpc": "2.0",
-            "method": "runtime_createExtrinsic",
-            "params": [
-                pair[0].address,
-                "Balances",
-                "transfer",
-                {
-                    "dest": pair[5].address,
-                    "value": 50
-                }
-            ],
-            "id": 1
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }
-    );
-    console.log('HTTP: ', test);
+export interface Params {
+    id: number,
+    jsonrpc: string,
+    method: string,
+    params?: number[] | string[]
+}
+export interface Result {
+    jsonrpc: string,
+    result: any,
+    id: number
 }
 
-main().then(
-    () => process.exit(),
-    err => {
-        console.error(err);
-        process.exit(-1);
-    }
-);
+export async function apiTransaction(pair: KeyringPair[], api: ApiPromise) {
+    const txHash = await api.tx.balances.transfer(pair[5].address, 1000).signAndSend(pair[0]);
+    console.log('Api Transaction: ', txHash);
+}
+
+export async function httpTransaction(pair: KeyringPair[]) {
+    const params: Params = {
+        id: 1,
+        jsonrpc: '2.0',
+        method: ''
+    };
+    const index = await httpRequest({ ...params, method: 'system_accountNextIndex', params: [pair[0].address] });
+    const { block } = await httpRequest({ ...params, method: 'chain_getBlock' });
+    const blockHash = await httpRequest({ ...params, method: 'chain_getBlockHash' });
+    const genesisHash = await httpRequest({ ...params, method: 'chain_getBlockHash', params: [0] });
+    const metadataRpc = await httpRequest({ ...params, method: 'state_getMetadata' });
+    const { specVersion, transactionVersion, specName } = await httpRequest({ ...params, method: 'state_getRuntimeVersion' });
+
+    const registry = getRegistry({
+        chainName: 'Polkadot',
+        specName,
+        specVersion,
+        metadataRpc,
+    });
+
+    const unsigned = methods.balances.transferKeepAlive(
+        {
+            value: '1000',
+            dest: pair[4].address,
+        },
+        {
+            address: pair[0].address,
+            blockHash,
+            blockNumber: registry
+                .createType('BlockNumber', block.header.number)
+                .toNumber(),
+            eraPeriod: 64,
+            genesisHash,
+            metadataRpc,
+            nonce: index,
+            specVersion,
+            tip: 0,
+            transactionVersion,
+        },
+        {
+            metadataRpc,
+            registry,
+        }
+    );
+
+    const signingPayload = construct.signingPayload(unsigned, { registry });
+
+    const signature = signWith(pair[0], signingPayload, {
+        metadataRpc,
+        registry,
+    });
+    const tx = construct.signedTx(unsigned, signature, {
+        metadataRpc,
+        registry,
+    });
+
+    console.log('Transaction: ', tx);
+    const transactionResult = await httpRequest({ ...params, method: 'author_submitExtrinsic', params: [tx] });
+    console.log('Transaction result: ', transactionResult);
+}
