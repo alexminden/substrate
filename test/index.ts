1c9e64842172bb2c2ef4d4ef14a8849a91433474
alex <alex@K041noMacBook-Pro.local>
2021-11-18 17:55:07 +0900

    Adapt to multiple node

--- test/index.ts
+++ test/index.ts
@@ -5,28 +5,34 @@ import BN from 'bn.js';
 
 async function main() {
     // Loop番号を変更することで、各アカウントの取引数を増減させることができます。
-    const loop = 5000;
+    const loop = 2000;
 
     const keyring = createTestKeyring();
     const pair = keyring.getPairs();
-    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+
+    const wsProvider = new WsProvider('ws://127.0.0.1:9945');
+    // const wsProvider2 = new WsProvider('ws://127.0.0.1:9946');
+
     const api: ApiPromise = await ApiPromise.create({ provider: wsProvider });
-    const now = await api.query.timestamp.now();
+    // const api2: ApiPromise = await ApiPromise.create({ provider: wsProvider2 });
 
     let data = (await api.query.system.account(pair[4].address)).data;
     console.log(`Account ${pair[4].address} balance is ${data.free}`);
 
+    const promises = [];
     const time = new Date().getTime();
+    // let apiTest;
     for (let i = 0; i < 4; i++) {
-        const promises = [];
+        // if(i % 2 == 0) apiTest = api;
+        // else apiTest = api2;
         let { nonce }: any = await api.query.system.account(pair[i].address);
         nonce = new BN(nonce.toString());
         for (let j = 0; j < loop; j++) {
             promises.push(apiTransaction(pair, api, i, 4, nonce));
             nonce = nonce.add(new BN(1));
         }
-        await Promise.all(promises);
     }
+    await Promise.all(promises);
     await calculateTPS(api, time, loop, 1);
 
     // console.log('HTTP');
